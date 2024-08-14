9fb87fd96b3f2a7610ca9d627ac79caaea57123d
Christian Mendonça <christian.lisboa.devlp@gmail.com>
2024-08-14 09:17:37 -0300

    initial project

--- revisao/index.js
+++ revisao/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const server = express();
 
-server.get('/curso', (req, res)=>{
+server.get('/curso/:id', (req, res)=>{
 
+    const id = req.params.id
     const nome = req.query.nome
 
-    return res.json({ curso : `Aprendendo ${nome}` })
+    return res.json(
+        {curso: `Curso de ${nome}: ${id}`}
+    )
 
 })
 
