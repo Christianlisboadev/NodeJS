531e334eb4f469468ea3c572bf85589f86f6c2c7
Christian Mendonça <christian.lisboa.devlp@gmail.com>
2024-08-16 22:54:53 -0300

    alterações de código, correções e imprementação de middlewares

--- exercicios/index.js
+++ exercicios/index.js
@@ -1,7 +1,8 @@
-
 const express = require('express');
 
 const server = express();
+
+server.use(express.json())
 
 // Query params = ?nome=NodeJS
 // Route params = /curso/2
@@ -12,11 +13,13 @@ const server = express();
 const cursos = ['NodeJS', 'JavaScript','PHP', 'React Native'];
 
 
+// Recuperando os dados de todos os cursos
 server.get('/cursos', (req, res)=>{
     return res.json(cursos)
 });
 
 
+// Recuperando os dados de um curso
 server.get('/cursos/:index', (req, res)=>{
 
     const {index} = req.params;
@@ -27,6 +30,7 @@ server.get('/cursos/:index', (req, res)=>{
 
 /// pesquisar post, request, push e .body
 
+// Adicionando um curso
 server.post(('/cursos'), (req, res)=>{
     const { name } = req.body;
     cursos.push(name);
@@ -34,5 +38,22 @@ server.post(('/cursos'), (req, res)=>{
     return res.json(cursos)
 });
 
+// Atualizando um curso
+server.put('/cursos/:index', (req, res)=>{
+    const { index } = req.params;
+    const { name } = req.body;
+
+    cursos[index] = name;
+
+    return res.json(cursos);
+});
+
+// Excluindo algum curso
+server.delete('/cursos/:index', (req, res)=>{
+    const {index} = req.params;
+
+    cursos.splice(index, 1)
+    return res.send();
+})
 
 server.listen(3000)
